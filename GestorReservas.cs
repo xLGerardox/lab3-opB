a227919345eda7730c1b155074937fe498cf02a9
Simon <124474593+Gerardo-Rivera@users.noreply.github.com>
2024-08-14 09:33:49 -0600

    GestorReervas

--- GestorReservas.cs
+++ GestorReservas.cs
@@ -1,1 +1,26 @@
-﻿
+﻿public class GestorReservas
+{
+    public List<Reserva> ListaReservas { get; set; } = new List<Reserva>();
+
+    public void RegistrarReserva(int numeroReserva, DateTime fecha, TimeSpan hora, Cliente cliente, List<Plato> platos)
+    {
+        var reserva = new Reserva(numeroReserva, fecha, hora, cliente);
+        reserva.ListaPlatos.AddRange(platos);
+        reserva.CalcularTotal();
+        ListaReservas.Add(reserva);
+    }
+
+    public Reserva BuscarReservaPorNumero(int numeroReserva)
+    {
+        return ListaReservas.FirstOrDefault(r => r.NumeroReserva == numeroReserva);
+    }
+
+    public void MostrarReservas()
+    {
+        foreach (var reserva in ListaReservas)
+        {
+            reserva.MostrarDetalles();
+            Console.WriteLine();
+        }
+    }
+}
