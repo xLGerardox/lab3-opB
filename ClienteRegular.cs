2dd93850fb10a5066a20dd751a3c49645231de8e
Simon <124474593+Gerardo-Rivera@users.noreply.github.com>
2024-08-14 09:05:15 -0600

    cambios en ClienteRegular

--- ClienteRegular.cs
+++ ClienteRegular.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OpcionB_lab3
+﻿public class ClienteRegular : Cliente
 {
-    internal class ClienteRegular
+    public ClienteRegular(string nombre, string correo, string telefono)
+        : base(nombre, correo, telefono)
     {
     }
 }
