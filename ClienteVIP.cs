9eb39136665bc90dcd2dcfe28bc491e628fb4c83
Simon <124474593+Gerardo-Rivera@users.noreply.github.com>
2024-08-14 09:06:31 -0600

    cambios en ClienteVIP

--- ClienteVIP.cs
+++ ClienteVIP.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OpcionB_lab3
+﻿public class ClienteVIP : Cliente
 {
-    internal class ClienteVIP
+    public float Descuento { get; set; }
+
+    public ClienteVIP(string nombre, string correo, string telefono, float descuento)
+        : base(nombre, correo, telefono)
     {
+        Descuento = descuento;
+    }
+
+    public void AplicarDescuento(ref float total)
+    {
+        total -= total * Descuento;
+    }
+
+    public override void MostrarDetalles()
+    {
+        base.MostrarDetalles();
+        Console.WriteLine($"Descuento: {Descuento * 100}%");
     }
 }
