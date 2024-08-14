1b4732f30d5a7a5b1fe77d0b94ac427ff56e7f1c
Simon <124474593+Gerardo-Rivera@users.noreply.github.com>
2024-08-14 09:16:36 -0600

    gestorCliente

--- GestorClientes.cs
+++ GestorClientes.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OpcionB_lab3
+﻿public class GestorClientes
 {
-    internal class GestorClientes
+    public List<Cliente> ListaClientes { get; set; } = new List<Cliente>();
+
+    public void RegistrarClienteRegular(string nombre, string correo, string telefono)
     {
+        var cliente = new ClienteRegular(nombre, correo, telefono);
+        ListaClientes.Add(cliente);
+    }
+
+    public void RegistrarClienteVIP(string nombre, string correo, string telefono, float descuento)
+    {
+        var cliente = new ClienteVIP(nombre, correo, telefono, descuento);
+        ListaClientes.Add(cliente);
+    }
+
+    public Cliente BuscarClientePorNombre(string nombre)
+    {
+        return ListaClientes.FirstOrDefault(c => c.Nombre.Equals(nombre, StringComparison.OrdinalIgnoreCase));
+    }
+
+    public void MostrarClientes()
+    {
+        foreach (var cliente in ListaClientes)
+        {
+            cliente.MostrarDetalles();
+            Console.WriteLine();
+        }
     }
 }
