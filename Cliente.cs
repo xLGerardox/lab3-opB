c0f6633fcd4a14c49e11194172bd69e22443ba7c
Simon <124474593+Gerardo-Rivera@users.noreply.github.com>
2024-08-14 09:02:55 -0600

    Cambios en Cliente

--- Cliente.cs
+++ Cliente.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OpcionB_lab3
+﻿public class Cliente
 {
-    internal class Cliente
+    public string Nombre { get; set; }
+    public string CorreoElectronico { get; set; }
+    public string NumeroTelefono { get; set; }
+
+    public Cliente(string nombre, string correo, string telefono)
     {
+        Nombre = nombre;
+        CorreoElectronico = correo;
+        NumeroTelefono = telefono;
+    }
+
+    public virtual void MostrarDetalles()
+    {
+        Console.WriteLine($"Nombre: {Nombre}, Correo: {CorreoElectronico}, Teléfono: {NumeroTelefono}");
     }
 }
