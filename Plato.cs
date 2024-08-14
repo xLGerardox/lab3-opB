8943980557891422812be66079a5c201cd02b57d
Simon <124474593+Gerardo-Rivera@users.noreply.github.com>
2024-08-14 08:57:48 -0600

    se crearon las clases necesarias

---
+++ Plato.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpcionB_lab3
+{
+    internal class Plato
+    {
+    }
+}
